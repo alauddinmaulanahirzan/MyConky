d3f014ab517bfb18a53f132d5ee309d5b6213d11
Alauddin Maulana Hirzan <maulanahirzan@gmail.com>
2022-03-12 17:23:30 +0700

    conky

--- Minimalis Conky/conkyx-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-killall conky
-sleep 5 && conky -c "/home/maulana/.conky/Minimalis Conky/conky.conf"
--- Minimalis Conky/pi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ping -c 1 -q 192.168.192.101 > /dev/null 2>&1; res=$?
-if [ $res -eq 0 ];then echo Connected;else echo Not Connected;fi
